0bde75c99d725ad6dd2b0059e4a8724ed2211d7f
sumeetmazumdar21 <sumeet.mazumdar.21@gmail.com>
2023-12-28 17:49:09 +0530

    Update script.js
    Adding animation to skill bars

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+// Scrolling to respective section on clicking the nav menu
 
 var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
 
@@ -19,4 +20,56 @@ for(var i = 0; i < navMenuAnchorTags.length; i++){
          }, 50);
     });
 
+}
+
+
+
+//  Setting the skill-bar width on reaching the skill-container
+
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillsContainer = document.getElementById("skills-container");
+window.addEventListener("scroll", checkScroll);
+var animationDone = false;
+
+function initialiseBars(){
+    for(let bar of progressBars){
+        bar.style.width = 0 + '%';
+    }
+}
+
+initialiseBars();
+
+function fillBars(){
+
+    for( let bar of progressBars){
+        let targetWidth = bar.getAttribute('data-bar-width');
+        let currentWidth = 0;
+        let interval = setInterval(function(){
+            if(currentWidth > targetWidth){
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + "%";
+        }, 15);
+    }
+}
+
+function checkScroll(){
+
+    // Checking first wheter the skill section is visible or not 
+
+    var coordinates = skillsContainer.getBoundingClientRect();
+
+    // if animation has not been intialise and skill section has been reached 
+
+    if(!animationDone && coordinates.top <= window.innerHeight){
+        animationDone = true;
+        fillBars();
+    }
+
+    else if(coordinates.top > window.innerHeight){
+        animationDone = false;
+        initialiseBars();
+    }
 }
