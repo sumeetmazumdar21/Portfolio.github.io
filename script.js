153c5c650a2815a24eb4cd518cd8bdcb7c2ff0a6
sumeetmazumdar21 <sumeet.mazumdar.21@gmail.com>
2023-12-28 14:52:16 +0530

    Add files via upload
    JavaScript for smooth scrolling to respective section

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+
+for(var i = 0; i < navMenuAnchorTags.length; i++){
+    navMenuAnchorTags[i].addEventListener('click', function(event){
+         event.preventDefault();
+
+         var targetSectionId = this.textContent.trim().toLowerCase();
+         var targetSection = document.getElementById(targetSectionId);
+         
+         var interval = setInterval(function(){
+            var targetSectionCoordinates = targetSection.getBoundingClientRect();
+            if(targetSectionCoordinates.top <= 0){
+                clearInterval(interval);
+                return;
+            }
+            window.scrollBy(0, 50);
+         }, 50);
+    });
+
+}
