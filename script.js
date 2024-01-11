15065320250ccd55d73b41d7dd5e7f8378bb4dae
sumeetmazumdar21 <sumeet.mazumdar.21@gmail.com>
2024-01-11 15:04:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,127 +1,75 @@
-// button identifiers
 
-let randomize = document.getElementById("randomize");
-let selection_sort = document.getElementById("selection-sort");
-let insertion_sort = document.getElementById("insertion-sort");
-let shell_sort = document.getElementById("shell-sort");
-let quick_sort = document.getElementById('quick-sort');
-let merge_sort = document.getElementById("merge-sort");
-let change_size = document.getElementById("change-size");
-let bubble_sort = document.getElementById("bubble-sort");
+// Scrolling to respective section on clicking the nav menu
 
-var sort_button=document.querySelectorAll(".sort");
-var bar_width = 10;
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
 
-var div_sizes=[];
-var divs=[];
-var margin_size;
-var cont=document.getElementById("array_container");
-cont.style="flex-direction:row";
+for(var i = 0; i < navMenuAnchorTags.length; i++){
+    navMenuAnchorTags[i].addEventListener('click', function(event){
+         event.preventDefault();
 
-// Array Creation
+         var targetSectionId = this.textContent.trim().toLowerCase();
+         var targetSection = document.getElementById(targetSectionId);
+         
+         var interval = setInterval(function(){
+            var targetSectionCoordinates = targetSection.getBoundingClientRect();
+            if(targetSectionCoordinates.top <= 0){
+                clearInterval(interval);
+                return;
+            }
+            window.scrollBy(0, 50);
+         }, 50);
+    });
 
-var min = 1;
-var max = 255;
-var arraySize = 50;
-
-let array = [arraySize];
-let sortedArray = [arraySize];
-
-
-randomize.addEventListener("click", generateArray);
-
-generateArray();
-
-
-
-function generateArray(){
-    for(var i = 0; i < arraySize; i++){
-        array[i] = Math.floor(Math.random() * (max- min) + min);;
-
-        if(array[i] == array[i - 1]){
-            array[i] =Math.floor(Math.random() * (max- min) + min);;
-        }
-    }
-
-    showBars();
 }
 
 
 
+//  Setting the skill-bar width on reaching the skill-container
 
-// Updating Array Size
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillsContainer = document.getElementById("skills-container");
+window.addEventListener("scroll", checkScroll);
+var animationDone = false;
 
-
-change_size.addEventListener("click", updateArraySize);
-change_size.addEventListener("click", decreaseArraySize);
-
-function updateArraySize(){
-    arraySize = 100;
-    generateArray();
-
-    change_size.addEventListener("click", decreaseArraySize);
-}
-
-function decreaseArraySize(){
-    arraySize = 50;
-    generateArray();
-
-    change_size.addEventListener("click", updateArraySize);
-}
-
-
-// Creating bars for the array
-
-function showBars(){
-
-    array_container.innerHTML= "";
-
-    for(let i = 0; i < array.length; i++){
-        const bar = document.createElement("div");
-        bar.style.height= array[i] + "px";
-        bar.classList.add("bar");
-        array_container.appendChild(bar);
+function initialiseBars(){
+    for(let bar of progressBars){
+        bar.style.width = 0 + '%';
     }
 }
 
+initialiseBars();
 
-// Sorting
+function fillBars(){
 
-function insertionSort(){
-    disable_buttons();
-    insertion(array);
-    showBars();
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Running the appropriate algorithm.
-
-selection_sort.addEventListener("click", selectionSort);
-
-
-function disable_buttons()
-{
-    for(var i=0;i<sort_button.length;i++)
-    {
-        sort_button[i].classList=[];
-        sort_button[i].classList.add("butt_locked");
-        sort_button[i].disabled=true;
-        randomize.disabled="true";
-        change_size.disabled="true";
-
-       
+    for( let bar of progressBars){
+        let targetWidth = bar.getAttribute('data-bar-width');
+        let currentWidth = 0;
+        let interval = setInterval(function(){
+            if(currentWidth > targetWidth){
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + "%";
+        }, 15);
     }
 }
 
+function checkScroll(){
+
+    // Checking first wheter the skill section is visible or not 
+
+    var coordinates = skillsContainer.getBoundingClientRect();
+
+    // if animation has not been intialise and skill section has been reached 
+
+    if(!animationDone && coordinates.top <= window.innerHeight){
+        animationDone = true;
+        fillBars();
+    }
+
+    else if(coordinates.top > window.innerHeight){
+        animationDone = false;
+        initialiseBars();
+    }
+}
